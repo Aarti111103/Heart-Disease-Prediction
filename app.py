7b1ed6c2c2afa3c43df2e4c25b18004cc88d257b
Aarti Agrahari <152190159+Aarti111103@users.noreply.github.com>
2025-01-09 10:01:56 +0530

    Update app.py
    Signed-off-by: Aarti Agrahari <152190159+Aarti111103@users.noreply.github.com>

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ except FileNotFoundError:
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('prediction.php')
 
 @app.route('/predict', methods=['POST'])
 def predict():
